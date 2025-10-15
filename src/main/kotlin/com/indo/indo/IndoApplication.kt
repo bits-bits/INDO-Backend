05a9e95c01b0816bbfa550afb79c6791bc2466fb
yousef_osama11 <oyousef709@gmail.com>
2025-10-15 18:01:33 +0300

    init commit

---
+++ src/main/kotlin/com/indo/indo/IndoApplication.kt
@@ -0,0 +1,11 @@
+package com.indo.indo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndoApplication
+
+fun main(args: Array<String>) {
+	runApplication<IndoApplication>(*args)
+}
