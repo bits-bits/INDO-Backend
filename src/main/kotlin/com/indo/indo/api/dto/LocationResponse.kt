cdf307d9a02f4ad421a01d332e0224a9fc0e66d7
yousef_osama11 <oyousef709@gmail.com>
2025-10-20 03:05:24 +0300

    feat: add location details response

---
+++ src/main/kotlin/com/indo/indo/api/dto/LocationResponse.kt
@@ -0,0 +1,11 @@
+package com.indo.indo.api.dto
+
+import com.fasterxml.jackson.core.sym.Name
+
+data class LocationDetailsResponse(
+    val id: String,
+    val name: String,
+    val aliasName: String?,
+    val buildingName: String,
+    val floorNumber: Int,
+)
