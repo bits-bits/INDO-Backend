5b52e6af8c303ed35caf8f00a42f038d854543f1
yousef_osama11 <oyousef709@gmail.com>
2025-10-20 03:06:54 +0300

    feat: add indo exceptions

---
+++ src/main/kotlin/com/indo/indo/exception/IndoExceptions.kt
@@ -0,0 +1,4 @@
+package com.indo.indo.exception
+
+class LocationNotFoundException(message: String) : RuntimeException(message)
+class InvalidIdFormException(message: String) : RuntimeException(message)
