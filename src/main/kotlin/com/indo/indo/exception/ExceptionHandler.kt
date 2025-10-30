5e7f2d00bf0e1d7a40ad546b7177443127be262d
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-10-30 03:32:01 +0300

    feat: add exceptions handler

---
+++ src/main/kotlin/com/indo/indo/exception/ExceptionHandler.kt
@@ -0,0 +1,24 @@
+package com.indo.indo.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException::class)
+	fun handleResourceNotFound(ex: ResourceNotFoundException): ResponseEntity<Map<String, String>> {
+		return ResponseEntity.status(404).body(mapOf("error" to "${ex.message}"))
+	}
+
+	@ExceptionHandler(InvalidIdFormException::class)
+	fun handleInvalidIdForm(ex: InvalidIdFormException): ResponseEntity<Map<String, String>> {
+		return ResponseEntity.status(404).body(mapOf("error" to "${ex.message}"))
+	}
+
+	@ExceptionHandler(Exception::class)
+	fun handleGenericException(ex: Exception): ResponseEntity<Map<String, String>> {
+		return ResponseEntity.status(500).body(mapOf("error" to "Internal Server Error"))
+	}
+}
