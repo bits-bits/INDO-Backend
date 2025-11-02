020e8addfae06dc1c3a44e8e1d8d24fb3cbbbb5a
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-11-03 01:13:55 +0200

    feat: add floor controller

---
+++ src/main/kotlin/com/indo/indo/api/controller/v1/FloorController.kt
@@ -0,0 +1,29 @@
+package com.indo.indo.api.controller.v1
+
+import com.indo.indo.api.dto.FloorBasicDetails
+import com.indo.indo.api.dto.FloorDetailsResponse
+import com.indo.indo.api.mapper.FloorMapper
+import com.indo.indo.exception.InvalidIdFormException
+import com.indo.indo.exception.ResourceNotFoundException
+import com.indo.indo.service.FloorService
+import com.indo.indo.util.UUIDUtils
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/v1/floor")
+class FloorController(private val floorService: FloorService, private val floorMapper: FloorMapper) {
+	@GetMapping("/{id}")
+	fun getFloorById(@PathVariable id: String): ResponseEntity<FloorDetailsResponse> {
+		if (!UUIDUtils.isValidUUIDString(id)) throw InvalidIdFormException("Invalid floor Id form")
+
+		val floor = floorService.findFloorById(UUID.fromString(id))
+			?: throw ResourceNotFoundException("Floor with given Id is not found")
+
+		return ResponseEntity.ok(floorMapper.toFloorDetailsResponse(floor))
+	}
+}
--- src/main/kotlin/com/indo/indo/api/mapper/FloorMapper.kt
+++ src/main/kotlin/com/indo/indo/api/mapper/FloorMapper.kt
@@ -1,6 +1,8 @@
 package com.indo.indo.api.mapper
 
 import com.indo.indo.api.dto.FloorBasicDetails
+import com.indo.indo.api.dto.FloorDetailsResponse
+import com.indo.indo.api.dto.LocationByFloorDetails
 import com.indo.indo.entity.Floor
 import org.springframework.stereotype.Component
 
@@ -8,4 +10,18 @@ import org.springframework.stereotype.Component
 class FloorMapper {
 	fun toBasicDetails(floor: Floor): FloorBasicDetails =
 		FloorBasicDetails(id = floor.id.toString(), number = floor.number)
+
+	fun toFloorDetailsResponse(floor: Floor): FloorDetailsResponse =
+		FloorDetailsResponse(
+			id = floor.toString(),
+			number = floor.number,
+			imageUrl = floor.imageUrl,
+			locations = floor.locations?.sortedBy { it.name }?.map {
+				LocationByFloorDetails(
+					id = it.id.toString(),
+					name = it.name,
+					aliasName = it.aliasName,
+				)
+			} ?: emptyList()
+		)
 }
