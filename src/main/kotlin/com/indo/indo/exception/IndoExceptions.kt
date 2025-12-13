45fe4c9d6eccc01e455d0a80273a1cf5dc4453a3
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-13 21:21:20 +0200

    feat: add general invalid form exception

--- src/main/kotlin/com/indo/indo/exception/IndoExceptions.kt
+++ src/main/kotlin/com/indo/indo/exception/IndoExceptions.kt
@@ -3,3 +3,4 @@ package com.indo.indo.exception
 class LocationNotFoundException(message: String) : RuntimeException(message)
 class ResourceNotFoundException(message: String) : RuntimeException(message)
 class InvalidIdFormException(message: String) : RuntimeException(message)
+class InvalidFormException(message: String) : RuntimeException(message)
