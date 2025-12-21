d8d1f943fb2c65228b6264fd6b826aa1acf8406f
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-12-21 23:05:11 +0200

    feat: add navigation controller for get route to location

--- src/main/kotlin/com/indo/indo/api/controller/v1/NavigationController.kt
+++ src/main/kotlin/com/indo/indo/api/controller/v1/NavigationController.kt
@@ -3,9 +3,10 @@ package com.indo.indo.api.controller.v1
 import com.indo.indo.api.dto.RouteDto
 import com.indo.indo.api.mapper.parseCoordinate
 import com.indo.indo.api.mapper.toRouteDto
-import com.indo.indo.entity.Coordinate
 import com.indo.indo.exception.InvalidFormException
+import com.indo.indo.service.LocationService
 import com.indo.indo.service.NavigationService
+import com.indo.indo.util.UUIDUtils
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,12 +17,25 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/route")
 class NavigationController(private val navigationService: NavigationService) {
 
-    @GetMapping
-    fun getRoute(@RequestParam("from") from:String, @RequestParam("to") to:String): ResponseEntity<RouteDto> {
+    @GetMapping("/coordinates")
+    fun getRoute(@RequestParam("from") from: String, @RequestParam("to") to: String): ResponseEntity<RouteDto> {
         val fromCoordinate = from.parseCoordinate()
         val toCoordinate = to.parseCoordinate()
 
         val route = navigationService.getRoute(fromCoordinate, toCoordinate).toRouteDto()
+        return ResponseEntity.ok(route)
+    }
+
+    @GetMapping("/location")
+    fun getRouteToLocation(
+        @RequestParam("from") from: String,
+        @RequestParam("toLocationId") toLocationId: String
+    ): ResponseEntity<RouteDto> {
+        val fromCoordinate = from.parseCoordinate()
+        val locationId =
+            UUIDUtils.getUuidOrNull(toLocationId) ?: throw InvalidFormException("Invalid coordinate format")
+
+        val route = navigationService.getRouteToLocation(fromCoordinate, locationId).toRouteDto()
         return ResponseEntity.ok(route)
     }
 }
